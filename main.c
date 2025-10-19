6c439f84a9a06cb15b60a315ac3df4e815f60255
Bryce Brantley <bryce.c.brantley@gmail.com>
2025-10-19 18:01:27 -0500

    Some changes from a while ago

--- main.c
+++ main.c
@@ -4,8 +4,19 @@
 
 int main() {
     stdio_init_all();
-    tusb_init();
-    printf("Hello World! \n");
+    tud_init(BOARD_TUD_RHPORT);
+    //Initialize ADC (Stage 2)
+    //Initialize the USB/Midi Task as ISR (Stage 1)
+    while(1)
+    {
+      uint8_t midiMsg[4] = {0,0,0,0};
+      printf("Hello World! \n");
+      tud_midi_packet_write(midiMsg);
+      //Continuously send midi message packet
+      busy_wait_ms(1000);
+    }
+        
     return 0;
 }
 
+// Need to implement some usbd callback functions 
--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -155,8 +155,8 @@ enum {
 char const *string_desc_arr[] =
 {
   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB Device",              // 2: Product
+  "Quiss Inc.",                     // 1: Manufacturer
+  "Midi Fader",              // 2: Product
   NULL,                          // 3: Serials will use unique ID if possible
 };
 
